607b6bd320a99d22a11a1a9af760dfe09850ddf3
Badirkhanov Rovshan Mukhlis <badirkhanov_r@itstep.edu.az>
2023-10-04 12:08:14 +0400

    add api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Paragraph from './Components/Paragraph/Paragraph';
+import {useGetShipsQuery} from './Redux/api';
 
 function App() {
+    const {data, isLoading} = useGetShipsQuery();
+
+    useEffect(() => {
+
+        if (isLoading) return;
+
+        console.log(data);
+
+    }, [isLoading]);
+
     return (
-        <Paragraph text={'Hello React'}/>
+        <Paragraph text={isLoading ? 'LOADING...' : 'Hello React'}/>
     )
 }
 
